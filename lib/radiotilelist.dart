6ff9336a33131e651728f5d79fb788894132649d
Ketan <ketantoo@gmail.com>
2020-06-08 17:48:15 +0530

    RadioListTiles snapshot

--- lib/radiotilelist.dart
+++ lib/radiotilelist.dart
@@ -10,45 +10,89 @@ class _RadioTileCenterState extends State<RadioTileCenter> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: Text("Center RadioTile")),
-      body: Center(
-        child: Container(
-            width: 200,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        home: Builder(
+      builder: (context) => Scaffold(
+        appBar: AppBar(title: Text("Center RadioTile")),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Color.fromRGBO(20, 124, 148, 1),
+          child: Center(
+            child: Stack(
               children: <Widget>[
-                RadioListTile(
-                  value: 0,
-                  groupValue: groupValue,
-                  title: Text(
-                    "Surah",
-                    style: TextStyle(color: Colors.green),
+                Container(
+                  width: 180,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RadioListTile(
+                        value: 0,
+                        title: Text("Surah"),
+                        groupValue: groupValue,
+                        onChanged: (v) {
+                          setState(() {
+                            groupValue = v;
+                          });
+                        },
+                        activeColor: Colors.white,
+                      ),
+                      RadioListTile(
+                        value: 1,
+                        title: Text("Identicals"),
+                        groupValue: groupValue,
+                        onChanged: (v) {
+                          setState(() {
+                            groupValue = v;
+                          });
+                        },
+                        activeColor: Colors.white,
+                      )
+                    ],
                   ),
-                  onChanged: (v) {
-                    setState(() {
-                      groupValue = v;
-                    });
-                  },
-                  activeColor: Colors.green,
                 ),
-                RadioListTile(
-                  value: 1,
-                  groupValue: groupValue,
-                  title: Text(
-                    "Identicals",
-                    style: TextStyle(color: Colors.green),
-                  ),
-                  onChanged: (v) {
-                    setState(() {
-                      groupValue = v;
-                    });
-                  },
-                  activeColor: Colors.green,
-                )
+                // CustomBottomSheet(),
               ],
-            )),
+            ),
+          ),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: () {
+            _settingModalBottomSheet(context);
+          },
+          child: new Icon(Icons.add),
+        ),
       ),
     ));
   }
+}
+
+void _settingModalBottomSheet(context) {
+  showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (BuildContext bc) {
+        print("hi");
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: new Column(
+            children: <Widget>[
+              new ListTile(
+                  leading: new Icon(Icons.music_note),
+                  title: new Text('Music'),
+                  onTap: () => {}),
+              new ListTile(
+                leading: new Icon(Icons.videocam),
+                title: new Text('Video'),
+                onTap: () => {},
+              ),
+            ],
+          ),
+        );
+      });
 }
